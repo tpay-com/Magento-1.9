a85d2aeac43bc6186713c587305035bafd046aef
Piotr Jóźwiak <piotr.jozwiak@tpay.com>
2017-06-09 14:27:36 +0200

    Method display improvement

--- app/code/local/Tpay/Tpay/Block/Form.php
+++ app/code/local/Tpay/Tpay/Block/Form.php
@@ -5,6 +5,8 @@ class Tpay_Tpay_Block_Form extends Mage_Payment_Block_Form {
   protected function _construct() {
     parent::_construct();
     $this->setTemplate('tpay/tpay/form.phtml');
+    $this->setMethodTitle($this->__('Zapłać wygodnie online z '));
+    $this->setMethodLabelAfterHtml('<img src="https://tpay.com/img/banners/tpay-przezroczyste-logo-85x24.png" />');
   }
 
   public function getPaymentImageSrc() {
